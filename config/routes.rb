cb28843b69acb9ab97a4b0eb0ffb35a4e290afcb
Monisha Gunalan <moni.sind@gmail.com>
2016-01-14 11:32:39 -0500

    added comment model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :blogposts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -7,7 +8,6 @@ Rails.application.routes.draw do
   # root 'welcome#index'
     root 'blogposts#index'
 
-      get 'hello'=> 'application#hello'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114152104) do
+ActiveRecord::Schema.define(version: 20160114162320) do
 
   create_table "blogposts", force: :cascade do |t|
     t.string   "title"
     t.text     "contents"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "commenttext"
+    t.string   "author"
+    t.integer  "blogpost_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
